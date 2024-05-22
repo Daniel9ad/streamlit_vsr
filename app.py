fdb2650a5c441e67d9e9b9b58aa34c4c4672f90c
Daniel <102867962+Daniel9ad@users.noreply.github.com>
2024-05-22 17:24:43 +0000

    app

---
+++ app.py
@@ -0,0 +1,27 @@
+import streamlit as st 
+import torchvision
+from gradio_client import Client, file
+#from streamlit_webrtc import webrtc_streamer
+
+# Cliente huggingface
+client = Client("Daniel31415/VSR-Castellano")
+
+st.header("Sistema de reconocimiento visual del habla VSR")
+
+# Camara
+#webrtc_streamer(key="sample")
+
+uploaded_file = st.file_uploader("Cargar video", type=['mp4'])
+if uploaded_file is not None:
+    st.video(uploaded_file)
+    # Guardar archivo
+    with open('clip.mp4', "wb") as f:
+        f.write(uploaded_file.read())
+    # Prediccion
+    with st.spinner('Transcribiendo...'):
+         roi, transcript = client.predict(
+        		video_path={"video":file('clip.mp4')},
+        		api_name="/generar_resultados_demo"
+        )
+    st.success(transcript)
+    st.video(roi['video'])
