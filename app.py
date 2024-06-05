a3421c9abfab925310eca0ba8a2d66bcfc917d3c
Daniel <102867962+Daniel9ad@users.noreply.github.com>
2024-06-05 00:11:04 +0000

    app

--- app.py
+++ app.py
@@ -21,9 +21,22 @@ from streamlit_webrtc import webrtc_streamer
 #         )
 #     st.success(transcript)
 #     st.video(roi['video'])
-
+# 
 # Camara
-st.header("Grabar")
-webrtc_streamer(
-    key="example"
-)
+# st.header("Grabar")
+# webrtc_streamer(
+#     key="example"
+# )
+from streamlit_webrtc import webrtc_streamer
+import av
+
+
+def video_frame_callback(frame):
+    img = frame.to_ndarray(format="bgr24")
+
+    flipped = img[::-1,:,:]
+
+    return av.VideoFrame.from_ndarray(flipped, format="bgr24")
+
+
+webrtc_streamer(key="example", video_frame_callback=video_frame_callback)
