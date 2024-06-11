2ce54aa2d96ce6b610e6ea892433dcf283f0472c
Daniel <102867962+Daniel9ad@users.noreply.github.com>
2024-06-11 19:48:43 +0000

    app

--- pages/grabar.py
+++ pages/grabar.py
@@ -1,6 +1,6 @@
 import streamlit as st 
 
 html_string = '<iframe src="https://daniel31415-vsr-castellano.hf.space" frameborder="0" width="850" height="450"></iframe>'
-st.html(html_string)
+#st.html(html_string)
 
-# st.markdown(html_string, unsafe_allow_html=True)
+st.markdown(html_string, unsafe_allow_html=True)
