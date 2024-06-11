f7a02d530049856cd6972f4e8c3ea5873cc90cd3
Daniel <102867962+Daniel9ad@users.noreply.github.com>
2024-06-11 19:59:50 +0000

    app

--- pages/grabar.py
+++ pages/grabar.py
@@ -1,6 +1,6 @@
 import streamlit as st 
 
-html_string = '<script type="module" src="https://gradio.s3-us-west-2.amazonaws.com/4.31.4/gradio.js"></script><gradio-app src="https://daniel31415-vsr-castellano.hf.space"></gradio-app>'
+html_string = '<iframe src="https://daniel31415-vsr-castellano.hf.space" frameborder="0" width="850" height="450"></iframe>'
 #st.html(html_string)
 
 st.markdown(html_string, unsafe_allow_html=True)
