dfff974d1d584ebe13b6ce728713e0ee00ab5af0
Daniel <102867962+Daniel9ad@users.noreply.github.com>
2024-06-11 19:38:41 +0000

    app

---
+++ pages/grabar.py
@@ -0,0 +1,5 @@
+import st
+
+html_string = '<iframe src="https://daniel31415-vsr-castellano.hf.space" frameborder="0" width="850" height="450"></iframe>'
+
+st.markdown(html_string, unsafe_allow_html=True)
