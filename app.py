c51801b8edcd612433ce7304f5b44022354ab805
Daniel <102867962+Daniel9ad@users.noreply.github.com>
2024-06-11 19:33:17 +0000

    app

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ st.header("Sistema de reconocimiento visual del habla VSR")
 
 st.page_link("app.py", label="Home", icon="🏠")
 st.page_link("pages/cargar.py", label="Cargar video", icon="1️⃣")
-st.page_link("pages/real_time.py", label="Real time", icon="2️⃣", disabled=True)
+st.page_link("pages/grabar.py", label="Grabar video", icon="2️⃣", disabled=True)
